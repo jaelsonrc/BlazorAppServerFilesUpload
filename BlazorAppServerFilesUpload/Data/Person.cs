ca6f5b9f962617f18bafb5aa6cde7279942093c3
jaels <jaels@NITRO5JAY>
2022-12-14 11:58:40 -0400

    Adicionar arquivos de projeto.

---
+++ BlazorAppServerFilesUpload/Data/Person.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAppServerFilesUpload.Data
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+
+        public IBrowserFile[] Picture { get; set; }
+    }
+}
